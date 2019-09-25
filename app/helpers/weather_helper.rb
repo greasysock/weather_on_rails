5359c5c0b1f7bf79fdf760d28a5b64ea25da0429
chris <chris.gresock@gmail.com>
2019-09-25 11:02:52 -0400

    Setup initial view

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,2 +1,20 @@
 module WeatherHelper
+    def five_day_render
+        out_days = ''
+        tomorrow = Date.tomorrow
+        5.times do |i|
+            out_days << content_tag(:div, class: 'secondary_weather_container') do
+                out_day = ''
+                out_day << content_tag(:div, class: 'temp', id: i+1) do
+                    ('--&deg').html_safe
+                end
+                out_day << content_tag(:div, class: 'day') do
+                    tomorrow.strftime("%A")
+                end
+                out_day.html_safe
+            end
+            tomorrow = tomorrow.tomorrow
+        end
+        out_days.html_safe
+    end
 end
