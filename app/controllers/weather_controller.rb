870ee2c270398a0c61a7cf4566e4db11778d7be0
chris <chris.gresock@gmail.com>
2019-09-25 11:33:55 -0400

    Added weather location route to rails

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,13 @@
 class WeatherController < ApplicationController
     def index
     end
+    def location
+        puts '*'*100
+        puts location_params
+        puts '*'*100
+    end
+    private
+    def location_params
+        params.permit(:lat, :lon)
+    end
 end
--- app/javascript/packs/weather.js
+++ app/javascript/packs/weather.js
@@ -1,5 +1,13 @@
-const OnReady = () => {
-    console.log('ready')
+import $ from 'jquery'
+
+const fetchWeather = async (pos) => {
+    const {latitude, longitude} = pos.coords
+    const weather = await fetch(`/weather?lat=${latitude}&lon=${longitude}`)
+    const results = await weather.json()
+    console.log(results)
 }
 
-document.onreadystatechange(OnReady)
+const OnReady = () => {
+    navigator.geolocation.getCurrentPosition(fetchWeather)
+}
+$(document).ready(OnReady)
